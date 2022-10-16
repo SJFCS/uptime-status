2527fe05be159a7103e48cb0782e44eef2d397e6
SongJinfeng <song.jinfeng@outlook.com>
2022-10-16 11:01:20 +0800

    change token

--- public/config.js
+++ public/config.js
@@ -6,10 +6,7 @@ window.Config = {
   // UptimeRobot Api Keys
   // 支持 Monitor-Specific 和 Read-Only
   ApiKeys: [
-    'm784488775-dd1ad84b209c05f8e185c33e',
-    'm784490063-7b5da437e7f1e0d67613714d',
-    'm784497419-de55aa09902ccb3ab22d548a',
-    'm784496436-71a4bf7b1e3bdf7756be131b',
+    'ur1883125-0968e16a0f96b4f3e718f99e',
   ],
 
   // 日志天数
@@ -22,15 +19,11 @@ window.Config = {
   Navi: [
     {
       text: 'Homepage',
-      url: 'https://status.org.cn/'
-    },
+      url: 'https://www.cloudnative.love/'
+    },   
     {
       text: 'GitHub',
-      url: 'https://github.com/yb/uptime-status'
-    },
-    {
-      text: 'Blog',
-      url: 'https://abo.xyz/'
+      url: 'https://github.com/SJFCS/uptime-status'
     },
   ],
 };
